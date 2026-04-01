af0c86c4d78c851762b67d0880319d244d547801
sandranielsen <eaasmmn@students.eaaa.dk>
2026-04-01 16:32:11 +0200

    Justering af mobilmenu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -14,27 +14,37 @@ export default function MobileMenu({ isOpen, onClose }) {
       {/* Menu */}
       <div className={`mobile-menu ${isOpen ? "open" : ""}`}>
         <div className="menu-content">
-          <button className="close-btn" onClick={onClose}>
-            <img src={close} className="close-icon"></img>
-          </button>
+          <div className="menu-header">
+            <h6>Menu</h6>
+            <button className="close-btn" onClick={onClose}>
+              <img src={close} className="close-icon"></img>
+            </button>
+          </div>
 
-          <ul>
-            <li>
-              <Link to="/#projekter" onClick={onClose}>
-                Projekter
-              </Link>
-            </li>
-            <li>
-              <Link to="/#om" onClick={onClose}>
-                Om
-              </Link>
-            </li>
-            <li>
-              <Link to="/#kontakt" onClick={onClose}>
-                Kontakt
-              </Link>
-            </li>
-          </ul>
+          <div className="menu-links">
+            <ul>
+              <li>
+                <Link to="/#projekter" onClick={onClose}>
+                  Projekter
+                </Link>
+              </li>
+              <li>
+                <Link to="/#om" onClick={onClose}>
+                  Om
+                </Link>
+              </li>
+              <li>
+                <Link to="/#kontakt" onClick={onClose}>
+                  Kontakt
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        
+        <div className="contact-links">
+          <Link to="mailto:sandramago@outlook.dk">sandramago@outlook.dk</Link>
+          <Link to="tel:+4522947700">+45 22 94 77 00</Link>
         </div>
       </div>
     </>
